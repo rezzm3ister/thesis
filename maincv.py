9bb44d976ef43c1bc2094350f4aae49602e538aa
Luis Alvarez <mlpa16.2@gmail.com>
2022-10-18 21:14:19 +0800

    added depth grid

--- maincv.py
+++ maincv.py
@@ -27,6 +27,87 @@ def getport():
   ports=list(serial.tools.list_ports.comports())
   return ports[0].device
 
+def getdispsum(depth):
+  dmx=vx//8
+  dmy=vy//8
+  dmat=np.zeros((8,8))
+
+  dmat[0,0]=np.sum(depth[0:17,0:31])
+  dmat[0,1]=np.sum(depth[0:17,32:63])
+  dmat[0,2]=np.sum(depth[0:17,64:95])
+  dmat[0,3]=np.sum(depth[0:17,96:127])
+  dmat[0,4]=np.sum(depth[0:17,128:159])
+  dmat[0,5]=np.sum(depth[0:17,160:191])
+  dmat[0,6]=np.sum(depth[0:17,192:221])
+  dmat[0,7]=np.sum(depth[0:17,224:255])
+
+  dmat[1,0]=np.sum(depth[18:35,0:31])
+  dmat[1,1]=np.sum(depth[18:35,32:63])
+  dmat[1,2]=np.sum(depth[18:35,64:95])
+  dmat[1,3]=np.sum(depth[18:35,96:127])
+  dmat[1,4]=np.sum(depth[18:35,128:159])
+  dmat[1,5]=np.sum(depth[18:35,160:191])
+  dmat[1,6]=np.sum(depth[18:35,192:221])
+  dmat[1,7]=np.sum(depth[18:35,224:255])
+
+  dmat[2,0]=np.sum(depth[36:53,0:31])
+  dmat[2,1]=np.sum(depth[36:53,32:63])
+  dmat[2,2]=np.sum(depth[36:53,64:95])
+  dmat[2,3]=np.sum(depth[36:53,96:127])
+  dmat[2,4]=np.sum(depth[36:53,128:159])
+  dmat[2,5]=np.sum(depth[36:53,160:191])
+  dmat[2,6]=np.sum(depth[36:53,192:221])
+  dmat[2,7]=np.sum(depth[36:53,224:255])
+
+  dmat[3,0]=np.sum(depth[54:71,0:31])
+  dmat[3,1]=np.sum(depth[54:71,32:63])
+  dmat[3,2]=np.sum(depth[54:71,64:95])
+  dmat[3,3]=np.sum(depth[54:71,96:127])
+  dmat[3,4]=np.sum(depth[54:71,128:159])
+  dmat[3,5]=np.sum(depth[54:71,160:191])
+  dmat[3,6]=np.sum(depth[54:71,192:221])
+  dmat[3,7]=np.sum(depth[54:71,224:255])
+
+  dmat[4,0]=np.sum(depth[72:89,0:31])
+  dmat[4,1]=np.sum(depth[72:89,32:63])
+  dmat[4,2]=np.sum(depth[72:89,64:95])
+  dmat[4,3]=np.sum(depth[72:89,96:127])
+  dmat[4,4]=np.sum(depth[72:89,128:159])
+  dmat[4,5]=np.sum(depth[72:89,160:191])
+  dmat[4,6]=np.sum(depth[72:89,192:221])
+  dmat[4,7]=np.sum(depth[72:89,224:255])
+
+  dmat[5,0]=np.sum(depth[90:125,0:31])
+  dmat[5,1]=np.sum(depth[90:125,32:63])
+  dmat[5,2]=np.sum(depth[90:125,64:95])
+  dmat[5,3]=np.sum(depth[90:125,96:127])
+  dmat[5,4]=np.sum(depth[90:125,128:159])
+  dmat[5,5]=np.sum(depth[90:125,160:191])
+  dmat[5,6]=np.sum(depth[90:125,192:221])
+  dmat[5,7]=np.sum(depth[90:125,224:255])
+
+  dmat[6,0]=np.sum(depth[126:143,0:31])
+  dmat[6,1]=np.sum(depth[126:143,32:63])
+  dmat[6,2]=np.sum(depth[126:143,64:95])
+  dmat[6,3]=np.sum(depth[126:143,96:127])
+  dmat[6,4]=np.sum(depth[126:143,128:159])
+  dmat[6,5]=np.sum(depth[126:143,160:191])
+  dmat[6,6]=np.sum(depth[126:143,192:221])
+  dmat[6,7]=np.sum(depth[126:143,224:255])
+
+  dmat[7,0]=np.sum(depth[126:144,0:31])
+  dmat[7,1]=np.sum(depth[126:144,32:63])
+  dmat[7,2]=np.sum(depth[126:144,64:95])
+  dmat[7,3]=np.sum(depth[126:144,96:127])
+  dmat[7,4]=np.sum(depth[126:144,128:159])
+  dmat[7,5]=np.sum(depth[126:144,160:191])
+  dmat[7,6]=np.sum(depth[126:144,192:221])
+  dmat[7,7]=np.sum(depth[126:144,224:255])
+
+  return dmat
+
+
+
 #ARDUINO CONNECT, UNCOMMENT LINE BELOW FOR ARDUINO
 #ardu=serial.Serial(port=getport(),baudrate=9600,timeout=1)
 
@@ -84,30 +165,13 @@ if __name__ == "__main__":
 
 
 
-    '''
-    depth matrix regions
-
-    a 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    -------------------------------
-    b 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    ------------------------------
-    c 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    ------------------------------
-    d 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    ------------------------------
-    e 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    ------------------------------
-    f 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    ------------------------------
-    g 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    ------------------------------
-    h 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-    '''
+    
     #DEPTH MAP
     dfl=cv.resize(fl,(vx,vy))
     dfr=cv.resize(fr,(vx,vy))
     stereo=cv.cuda.StereoSGM.create(minDisparity=10,numDisparities=32,blockSize=16,speckleRange=4)
     depth=stereo.compute(dfl,dfr)
+    depth=depth
     cv.imshow("depth",depth/1280)
 
     #depth regions
@@ -115,15 +179,26 @@ if __name__ == "__main__":
     dmx=vx//8
     dmy=vy//8
 
-    for j in range(8):
-      for i in range(8):
-        td=depth[(dmx*j):(dmx*(j+1)),(dmx*i):(dmx*(i+1))]
-        dmat[j,i]=np.sum(td)
-        #print(j,"j,",dmx,", ",dmx*j, ", ",dmat[j,i])
-      #print(i,"i,",dmy,", ",dmy*i)
+    dmat=getdispsum(depth)
+
+    #td=np.zeros((144,256))
+
+    #td=depth[0:18,128:160]
+    #cv.imshow("td",td)
+
+       
+
+    
+    #for j in range(8):
+    #  for i in range(8):
+    #    td=depth[(dmx*j):(dmx*(j+1)-1),(dmx*i):(dmx*(i+1))-1]
+    #    cv.imshow("td",td)
+    #    dmat[j,i]=np.sum(td)
+    #    #print(j,"j,",dmx,", ",dmx*j, ", ",dmat[j,i])
+    #  #print(i,"i,",dmy,", ",dmy*i)
 
 
-    print(depth)
+    print(dmat)
     print(" ")
     #detect closeness
 
