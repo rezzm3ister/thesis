57491be6fc34ca83aa1c4dc345c51c810dcdd84b
Luis Alvarez <mlpa16.2@gmail.com>
2022-10-18 20:39:48 +0800

    upd

--- maincv.py
+++ maincv.py
@@ -6,10 +6,9 @@
 
 import cv2 as cv
 import time
+import numpy as np
 import serial
 import serial.tools.list_ports
-
-
 
 #main cam res size
 sx=1280
@@ -84,12 +83,50 @@ if __name__ == "__main__":
     cv.imshow('img',DMSframe)
 
 
+
+    '''
+    depth matrix regions
+
+    a 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    -------------------------------
+    b 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    ------------------------------
+    c 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    ------------------------------
+    d 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    ------------------------------
+    e 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    ------------------------------
+    f 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    ------------------------------
+    g 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    ------------------------------
+    h 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+    '''
     #DEPTH MAP
     dfl=cv.resize(fl,(vx,vy))
     dfr=cv.resize(fr,(vx,vy))
     stereo=cv.cuda.StereoSGM.create(minDisparity=10,numDisparities=32,blockSize=16,speckleRange=4)
     depth=stereo.compute(dfl,dfr)
     cv.imshow("depth",depth/1280)
+
+    #depth regions
+    dmat=np.zeros((8,8))
+    dmx=vx//8
+    dmy=vy//8
+
+    for j in range(8):
+      for i in range(8):
+        td=depth[(dmx*j):(dmx*(j+1)),(dmx*i):(dmx*(i+1))]
+        dmat[j,i]=np.sum(td)
+        #print(j,"j,",dmx,", ",dmx*j, ", ",dmat[j,i])
+      #print(i,"i,",dmy,", ",dmy*i)
+
+
+    print(depth)
+    print(" ")
+    #detect closeness
+
 
 
 
@@ -99,6 +136,8 @@ if __name__ == "__main__":
     #cv.putText(depth, str(fps), (7, 70), font, 1, 255, 3, cv.LINE_AA)
     #print(fps)
     pft=ft
+
+
 
     
     #SEND DATA TO ARDUINO
